dc1fa66bd2840e3d268796ba53044b2e5d6f2908
Adityasharma3010 <Nakul.adi@gmail.com>
2025-10-08 13:57:52 +0530

    fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ import { ComputersCanvas } from "./canvas";
 const Hero = () => {
   const [textIndex, setTextIndex] = useState(0);
   const [displayedText, setDisplayedText] = useState("");
-  const texts = ["Web Developer,", "Game Developer,", "Software Developer,"];
+  const texts = ["Web Developer", "Game Developer", "Software Developer"];
 
   const charIndexRef = useRef(0);
   const isDeletingRef = useRef(false);
@@ -87,7 +87,7 @@ const Hero = () => {
 
       <ComputersCanvas />
 
-      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+      <div className="absolute flex items-center justify-center w-full xs:bottom-10 bottom-32">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
@@ -99,7 +99,7 @@ const Hero = () => {
                 repeat: Infinity,
                 repeatType: "loop",
               }}
-              className="w-3 h-3 rounded-full bg-secondary mb-1"
+              className="w-3 h-3 mb-1 rounded-full bg-secondary"
             />
           </div>
         </a>
