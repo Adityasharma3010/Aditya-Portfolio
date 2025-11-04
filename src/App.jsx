9ff7b9d5afe479869f3c713725622c0e2a0e07a7
Adityasharma3010 <66372496+Adityasharma3010@users.noreply.github.com>
2025-11-04 15:54:13 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ const App = () => {
         </div>
         <About />
         <Experience />
-        
+        <Tech />
         <Works />
         {/* <Feedbacks /> */}
         <div className="relative z-0">
